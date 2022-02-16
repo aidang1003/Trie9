5547e24084f4079082308bcc6617ae6573b8215d
Aidan Gorny <aidang1003@gmail.com>
2022-02-15 21:55:00 -0600

    gitignore test

---
+++ trie.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "trie.h"
+
+/*
+struct TrieNode {
+	//define this
+	char word[MAX_CHARS_PER_WORD];
+	struct TrieNode* children[NUM_CHILDREN];
+};
+*/
+
+void foo() {
+    printf("Hello World");
+}
+
+main() {
+    foo();
+}
