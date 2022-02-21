aade0c655346aa3113eb0f896f24318a08261e93
Aidan Gorny <aidang1003@gmail.com>
2022-02-21 14:20:31 -0600

    app.c
    should print the contents of the file line by line

--- app.c
+++ app.c
@@ -5,10 +5,20 @@
 #include "trie.h"
 
 int main(int argc, char *argv[]) {
-    printf("%d\n", argc);
-    int len = argc;
-    for (int i = 0; i < len; i++) {
-        printf("string %d >> %s\n", i, argv[i]);
+    int bufferSize = 1000;
+
+    FILE *fp = fopen(argv[1], "r"); /* "r" = open for reading */
+
+    char buff[bufferSize]; /* a buffer to hold what you read in */
+
+    /* read in one line, up to bufferSize-1 in length */
+    while(fgets(buff, bufferSize - 1, fp) != NULL)
+    {
+        /* buff has one line of the file, do with it what you will... */
+
+        printf ("string >> %s\n", buff); /* ...such as show it on the screen */
     }
+    fclose(fp);  /* close the file */
+
     return 1;
 }
